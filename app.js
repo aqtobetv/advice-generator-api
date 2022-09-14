b2ad9ce6ddbd3a91207d4b49761829e514a4695d
arman2906 <88658233+arman2906@users.noreply.github.com>
2022-09-14 19:23:59 +0500

    button position changed

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const neonCircleButton = document.querySelector('.neonCircle')
 const advice = document.querySelector('.advice')
+const adviceNumber = document.querySelector('.advice-number')
 
-neonCircleButton.addEventListener('click', () => {
-    console.log("click")
-    fetchData()
+let visited = 1
+
+neonCircleButton.addEventListener('click', () => {       
+    fetchData()   
 })
 
-async function fetchData(){
+async function fetchData(){  
     const data = await fetch('https://api.adviceslip.com/advice')
-    const json = await data.json()
-    console.log(json)
+    const json = await data.json() 
     advice.textContent = json.slip.advice
-
+    adviceNumber.textContent = `ADVICE #${json.slip.id}`
+    adviceNumber.dataset.id = json.slip.id    
 }
